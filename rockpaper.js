c0b4f335f17227a50e28b84a118003353ec53b5f
Chris V <119519387+c1846946@users.noreply.github.com>
2022-12-20 14:31:22 -0500

    Get a random integer 1 - 3

---
+++ rockpaper.js
@@ -0,0 +1,11 @@
+
+//make a function that will randomly return rock paper or scissors
+
+function getComputerChoice() {
+    //get an integer 1-3 randomly
+    let num = Math.floor(Math.random()*3+1);
+    return num
+    //assign rock paper scissors 1-3
+}   
+console.log(getComputerChoice())
+
