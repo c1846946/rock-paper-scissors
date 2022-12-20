e6762f2f771bd967a196fc68ef5ad3fcb2c5680e
Chris V <119519387+c1846946@users.noreply.github.com>
2022-12-20 14:54:39 -0500

    Change 1,2,3 to Rock,Paper,Scissors

--- rockpaper.js
+++ rockpaper.js
@@ -4,8 +4,16 @@
 function getComputerChoice() {
     //get an integer 1-3 randomly
     let num = Math.floor(Math.random()*3+1);
-    return num
     //assign rock paper scissors 1-3
+    let rps
+    if (num === 1) {
+        rpsComp = "Rock"
+    } else if (num === 2){
+        rpsComp = "Paper"
+    } else {
+        rpsComp = "Scissors"
+    }
+    return rpsComp
 }   
 console.log(getComputerChoice())
 
