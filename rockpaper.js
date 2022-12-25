ee72f798e3fd6a6f19dd6894aa54b7a325425381
Chris V <119519387+c1846946@users.noreply.github.com>
2022-12-25 11:33:11 -0500

    Complete function for best of 5

--- rockpaper.js
+++ rockpaper.js
@@ -4,7 +4,7 @@
 function getComputerChoice() {
     //get an integer 1-3 randomly
     let num = Math.floor(Math.random()*3+1);
-    //assign rock paper scissors 1-3
+    //assign rock paper scissors to 1-3
     let rps
     if (num === 1) {
         rpsComp = "rock"
@@ -16,28 +16,54 @@ function getComputerChoice() {
     return rpsComp
 }   
 
-//get user input
-//--make user input case insensitive
-
 
 function playOneRound(rpsUser,computerSelection) {
-    //use a boolean
-    //=== draw
+    //get user input
+    //--make user input case insensitive
     rpsUser = prompt('Enter "Rock", "Paper", or "Scissors"',"Rock" ).toLowerCase()
     computerSelection = getComputerChoice()
+    let winner
 
     console.log(rpsUser)
     console.log(computerSelection)
-    
+    //use booleans
     if (rpsUser === computerSelection) {
-        return "It's a draw!"
-    } else if (rpsUser === "rock" && computerSelection === "paper" || rpsUser === "paper" && computerSelection === "rock" || rpsUser === "scissors" && computerSelection === "paper") {
-        return `You won! ${rpsUser} beats ${computerSelection}!`
+        return "draw"
+    } else if (rpsUser === "scissors" && computerSelection === "paper" || rpsUser === "paper" && computerSelection === "rock" || rpsUser === "rock" && computerSelection === "scissors") {
+        return "user"
     } else {
-        return `You lost! ${computerSelection} beats ${rpsUser}!`
+        return "comp"
     }
-    
+    //---modify to increment scores
     
 }
-console.log(playOneRound())
 
+function game() {
+    //starting score
+    let userScore = 0
+    let compScore = 0
+    //play five rounds
+    for (let i = 0; i < 5; i++) {
+        //run playOneRound 
+        let winner = playOneRound()
+        
+        if (winner === "user") {
+            userScore = ++userScore
+        } else if (winner === "comp") {
+            compScore = ++compScore
+        } else {}
+        //increment whoever won
+        console.log(`User: ${userScore} Computer: ${compScore}`)
+        //show score after this round
+    }
+    //if to declare when a winner reaches 5 points
+    if (userScore > compScore) {
+        return "You won!"
+    } else if (userScore === compScore) {
+        return "It's a draw!"
+    } else {
+        return "You lost! Womp, Womp"
+    }
+}
+
+console.log(game())
