47fb064abb616a003ecfc91d7e7cdfdc9817e85c
Chris V <119519387+c1846946@users.noreply.github.com>
2023-01-08 07:48:23 -0500

    Add selections above score

--- rockpaper.js
+++ rockpaper.js
@@ -2,8 +2,11 @@
 let userScore = 0
 let compScore = 0
 let numberOfDraws = 0
+
 const userSelDiv = document.querySelector('.user-selection')
 const compSelDiv = document.querySelector('.comp-selection')
+userSelDiv.textContent = ''
+compSelDiv.textContent = ''
 const userScoreDiv = document.querySelector('.user-score')
 const compScoreDiv = document.querySelector('.comp-score')
 const draw = document.querySelector('.draw')
@@ -12,9 +15,9 @@ const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 
 
-rock.addEventListener('click', () => playOneRound("rock", getComputerChoice()))
-paper.addEventListener('click', () => playOneRound("paper", getComputerChoice()))
-scissors.addEventListener('click', () => playOneRound("scissors", getComputerChoice()))
+rock.addEventListener('click', () => playOneRound("Rock", getComputerChoice()))
+paper.addEventListener('click', () => playOneRound("Paper", getComputerChoice()))
+scissors.addEventListener('click', () => playOneRound("Scissors", getComputerChoice()))
 
 
 
@@ -25,24 +28,27 @@ function getComputerChoice() {
     //assign rock paper scissors to 1-3
     let rps
     if (num === 1) {
-        rpsComp = "rock"
+        rpsComp = "Rock"
     } else if (num === 2){
-        rpsComp = "paper"
+        rpsComp = "Paper"
     } else {
-        rpsComp = "scissors"
+        rpsComp = "Scissors"
     }
     return rpsComp
 }   
 
 function playOneRound(rpsUser,computerSelection) {
     //compare user and computer selection to determine winner
-    
+    userSelDiv.textContent = rpsUser
+    compSelDiv.textContent = computerSelection
     if (rpsUser === computerSelection) {
         draw.textContent = "Draws: " + ++numberOfDraws
-    } else if (rpsUser === "scissors" && computerSelection === "paper" || rpsUser === "paper" && computerSelection === "rock" || rpsUser === "rock" && computerSelection === "scissors") {
+    } else if (rpsUser === "Scissors" && computerSelection === "Paper" || rpsUser === "Paper" && computerSelection === "Rock" || rpsUser === "Rock" && computerSelection === "Scissors") {
         userScoreDiv.textContent = "User: " + ++userScore
+        
     } else {
         compScoreDiv.textContent = "Computer: " + ++compScore
+        
     }
     
 }
