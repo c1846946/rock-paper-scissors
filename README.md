269655e782df30c1b0593d18015f3ab2f4b859e3
Chris V <119519387+c1846946@users.noreply.github.com>
2023-01-08 04:55:41 -0500

    Add buttons for user input and calling comp input

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+First js project in TOP
--- rockpaper.js
+++ rockpaper.js
@@ -16,54 +16,66 @@ function getComputerChoice() {
     return rpsComp
 }   
 
-
 function playOneRound(rpsUser,computerSelection) {
     //get user input
     //--make user input case insensitive
-    rpsUser = prompt('Enter "Rock", "Paper", or "Scissors"',"Rock" ).toLowerCase()
-    computerSelection = getComputerChoice()
+    
+    
+    
     let winner
 
-    console.log(rpsUser)
-    console.log(computerSelection)
+    
+    
     //use booleans
     if (rpsUser === computerSelection) {
         return "draw"
     } else if (rpsUser === "scissors" && computerSelection === "paper" || rpsUser === "paper" && computerSelection === "rock" || rpsUser === "rock" && computerSelection === "scissors") {
         return "user"
     } else {
-        return "comp"
+        console.log("comp")
     }
     //---modify to increment scores
     
 }
 
-function game() {
-    //starting score
-    let userScore = 0
-    let compScore = 0
-    //play five rounds
-    for (let i = 0; i < 5; i++) {
-        //run playOneRound 
-        let winner = playOneRound()
-        
-        if (winner === "user") {
-            userScore = ++userScore
-        } else if (winner === "comp") {
-            compScore = ++compScore
-        } else {}
-        //increment whoever won
-        console.log(`User: ${userScore} Computer: ${compScore}`)
-        //show score after this round
-    }
-    //if to declare when a winner reaches 5 points
-    if (userScore > compScore) {
-        return "You won!"
-    } else if (userScore === compScore) {
-        return "It's a draw!"
-    } else {
-        return "You lost! Womp, Womp"
-    }
+
+  function userChoice() {
+    const rock = document.querySelector('.rock');
+    const paper = document.querySelector('.paper');
+    const scissors = document.querySelector('.scissors');
+    rock.addEventListener('click', () => playOneRound("rock", getComputerChoice()))
+    paper.addEventListener('click', () => playOneRound("paper", getComputerChoice()))
+    scissors.addEventListener('click', () => playOneRound("scissors", getComputerChoice()))
 }
 
-console.log(game())
+console.log(userChoice())
+
+
+// function game() {
+//     //starting score
+//     let userScore = 0
+//     let compScore = 0
+//     //play five rounds
+//     for (let i = 0; i < 5; i++) {
+//         //run playOneRound 
+//         let winner = playOneRound()
+        
+//         if (winner === "user") {
+//             userScore = ++userScore
+//         } else if (winner === "comp") {
+//             compScore = ++compScore
+//         } else {}
+//         //increment whoever won
+//         console.log(`User: ${userScore} Computer: ${compScore}`)
+//         //show score after this round
+//     }
+//     //if to declare when a winner reaches 5 points
+//     if (userScore > compScore) {
+//         return "You won!"
+//     } else if (userScore === compScore) {
+//         return "It's a draw!"
+//     } else {
+//         return "You lost! Womp, Womp"
+//     }
+// }
+
