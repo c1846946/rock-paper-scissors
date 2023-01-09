ab56d9dbaab80998b59fbabc07ba8b527b191c2d
Chris V <119519387+c1846946@users.noreply.github.com>
2023-01-08 20:34:17 -0500

    Stop play at first to 5 points and declare winner

--- rockpaper.js
+++ rockpaper.js
@@ -5,20 +5,23 @@ let numberOfDraws = 0
 
 const userSelDiv = document.querySelector('.user-selection')
 const compSelDiv = document.querySelector('.comp-selection')
-userSelDiv.textContent = ''
-compSelDiv.textContent = ''
 const userScoreDiv = document.querySelector('.user-score')
 const compScoreDiv = document.querySelector('.comp-score')
 const draw = document.querySelector('.draw')
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
+const winnerDiv = document.querySelector('.winner')
+
+userScoreDiv.textContent = "User: " + userScore
+compScoreDiv.textContent = "Computer: " + compScore
+draw.textContent = "Draws: " + numberOfDraws
 
 
-rock.addEventListener('click', () => playOneRound("Rock", getComputerChoice()))
-paper.addEventListener('click', () => playOneRound("Paper", getComputerChoice()))
-scissors.addEventListener('click', () => playOneRound("Scissors", getComputerChoice()))
 
+ rock.addEventListener('click', playRock);
+ paper.addEventListener('click',playPaper);
+ scissors.addEventListener('click',playScissors);
 
 
 //make a function that will randomly return rock paper or scissors
@@ -36,6 +39,17 @@ function getComputerChoice() {
     }
     return rpsComp
 }   
+function playRock() {
+    playOneRound("Rock", getComputerChoice())
+}
+
+function playPaper() {
+    playOneRound("Paper", getComputerChoice())
+}
+
+function playScissors() {
+    playOneRound("Scissors", getComputerChoice())
+}
 
 function playOneRound(rpsUser,computerSelection) {
     //compare user and computer selection to determine winner
@@ -45,19 +59,32 @@ function playOneRound(rpsUser,computerSelection) {
         draw.textContent = "Draws: " + ++numberOfDraws
     } else if (rpsUser === "Scissors" && computerSelection === "Paper" || rpsUser === "Paper" && computerSelection === "Rock" || rpsUser === "Rock" && computerSelection === "Scissors") {
         userScoreDiv.textContent = "User: " + ++userScore
-        
+        return winner()
     } else {
         compScoreDiv.textContent = "Computer: " + ++compScore
-        
+        return winner()
     }
     
 }
 
-userScoreDiv.textContent = "User: " + userScore
-compScoreDiv.textContent = "Computer: " + compScore
-draw.textContent = "Draws: " + numberOfDraws
+function winner() {
+     if (userScore == 5) {
+        removeListeners()
+        winnerDiv.innerText = "You Won!!!";
+    } else if (compScore == 5) {
+        removeListeners()
+        winnerDiv.innerText = "Computer Won!!!"
+    }   else{
+        return
+    }
+}
 
-
+function removeListeners() {
+    rock.removeEventListener('click', playRock);
+    paper.removeEventListener('click', playPaper);
+    scissors.removeEventListener('click', playScissors);
+    console.log("listeners removed")
+}
 // function game() {
 //     //starting score
 //     let userScore = 0
